3165ae49232b0a7966d7a232fd31ecc44e3a0416
jayeshk-star <jayeshkale052@gmail.com>
2021-09-17 22:26:15 +0530

    all file

--- myapp/src/compoents/Actualchat.jsx
+++ myapp/src/compoents/Actualchat.jsx
@@ -1,15 +1,21 @@
 import './Actualchat.css'
 import { Avatar, IconButton } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
-import { AttachFile, SearchOutlined } from '@material-ui/icons'
+import { AttachFile, SearchOutlined,InsertEmoticon } from '@material-ui/icons'
 import MoreVert from '@material-ui/icons/MoreVert'
+import MicIcon from "@material-ui/icons/Mic"
 
 const Actualchat = () => {
   const [img, setimg] = useState('')
+  const [input,setinput]=useState("")
 
   useEffect(() => {
     setimg(Math.floor(Math.random() * 5000))
   })
+  const sendmassage=(e)=>{
+      e.preventDefault()
+      setinput("")
+  }
   return (
     <div className='headerchat'>
       <div className='Chatheader'>
@@ -30,8 +36,40 @@ const Actualchat = () => {
           </IconButton>
         </div>
       </div>
-      <div className='actalchat_middle'></div>
-      <div className='actualchat_lower'></div>
+      <div className='actalchat_middle'>
+        <p className={`chat_massage ${true && `chat_receiver`}` }>
+          <span className='chat_name'>jayesh</span>
+        hello what is your name
+          <span className='timestamp'>{new Date().toLocaleTimeString()}</span>
+        
+        </p>
+        <p className='chat_massage'>
+          <span className='chat_name'>jayesh</span>
+          <span className='timestamp'>{new Date().toLocaleTimeString()}</span>
+        </p>
+        <p className={`chat_massage ${true && `chat_receiver`}` }>
+          <span className='chat_name'>jayesh</span>
+        hello what is your name
+          <span className='timestamp'>{new Date().toLocaleTimeString()}</span>
+        
+        </p>
+        <p className='chat_massage'>
+          <span className='chat_name'>jayesh</span>
+          <span className='timestamp'>{new Date().toLocaleTimeString()}</span>
+        </p>
+      </div>
+      <div className='actualchat_lower'>
+        <InsertEmoticon />
+        <form>
+          <input
+          value={input}
+          onChange={e=>e.target.value}
+          placeholder="type a massage" type="text" />
+          <button type="submit" onClick={sendmassage}>Send a message</button>
+
+        </form>
+        <MicIcon />
+      </div>
     </div>
   )
 }
--- myapp/src/firebase.js
+++ myapp/src/firebase.js
@@ -1,3 +1,9 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/storage"
+import "firebase/database"
+import "firebase/firestore"
+
 const firebaseConfig = {
     apiKey: "AIzaSyB3qppq3o8Fc6dWs8OnPSYe598RgENywhQ",
     authDomain: "whatsapp-clone-2012f.firebaseapp.com",
@@ -6,3 +12,11 @@ const firebaseConfig = {
     messagingSenderId: "1080100005130",
     appId: "1:1080100005130:web:8991bfa70bb934a146c6f3"
   };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig)
+  const db=firebaseApp.store()
+  const auth=firebase.auth()
+  const provider=new firebase.auth.GoogleAuthProvider()
+
+  export {auth,provider}
+  export default db
